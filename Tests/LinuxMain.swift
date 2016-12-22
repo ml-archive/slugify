1ca2eecb1ba872cd7fcfee4d663040a4fd025171
Casper Rasmussen <cr@nodes.dk>
2016-12-22 20:41:47 +0100

    deleted tests

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import StorageTests
-
-XCTMain([
-    testCase(StorageTests.allTests), 
-    testCase(FileEntityTests.allTests),
-    testCase(TemplateTests.allTests),
-    testCase(PathBuilderTests.allTests)
-])
