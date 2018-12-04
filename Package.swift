2329b2d72f43f0764fa5b935bf77fbc50d7bf71e
Heidi Puk Hermann <heih@nodesagency.com>
2018-12-04 15:08:35 +0100

    Update Swift Package Manifest to V.4 and Swift to 4.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,17 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Slugify",
+    products: [
+        .library(name: "Slugify", targets: ["Slugify"]),
+    ],
     dependencies: [
+    ],
+    targets: [
+        .target(name: "Slugify", dependencies: []),
+        .testTarget(name: "SlugifyTests", dependencies: ["Slugify"])
     ]
 )
+
